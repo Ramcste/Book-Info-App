dc536b5359afae3ef3ac14bb4b151528111741c1
Ramcste <lucky.guyram@gmail.com>
2015-04-25 23:22:04 +0600

    Book Info App

---
+++ BookInfoApp/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace BookInfoApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.addBookButton = new System.Windows.Forms.Button();
+            this.findBookButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.isbnTextBox = new System.Windows.Forms.TextBox();
+            this.bookDetailsTextBox = new System.Windows.Forms.TextBox();
+            this.isbnBox = new System.Windows.Forms.TextBox();
+            this.bookDetailsBox = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.bookDetailsTextBox);
+            this.groupBox1.Controls.Add(this.isbnTextBox);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.addBookButton);
+            this.groupBox1.Location = new System.Drawing.Point(54, 29);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(431, 164);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Add Book Info";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.bookDetailsBox);
+            this.groupBox2.Controls.Add(this.isbnBox);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.findBookButton);
+            this.groupBox2.Location = new System.Drawing.Point(54, 217);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(431, 165);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Search Book";
+            // 
+            // addBookButton
+            // 
+            this.addBookButton.Location = new System.Drawing.Point(337, 118);
+            this.addBookButton.Name = "addBookButton";
+            this.addBookButton.Size = new System.Drawing.Size(75, 23);
+            this.addBookButton.TabIndex = 0;
+            this.addBookButton.Text = "Add";
+            this.addBookButton.UseVisualStyleBackColor = true;
+            this.addBookButton.Click += new System.EventHandler(this.addBookButton_Click);
+            // 
+            // findBookButton
+            // 
+            this.findBookButton.Location = new System.Drawing.Point(337, 19);
+            this.findBookButton.Name = "findBookButton";
+            this.findBookButton.Size = new System.Drawing.Size(75, 23);
+            this.findBookButton.TabIndex = 0;
+            this.findBookButton.Text = "Find";
+            this.findBookButton.UseVisualStyleBackColor = true;
+            this.findBookButton.Click += new System.EventHandler(this.findBookButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(79, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ISBN";
+           
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(44, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Book Details";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(59, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "ISBN";
+      
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 77);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Book Details";
+            // 
+            // isbnTextBox
+            // 
+            this.isbnTextBox.Location = new System.Drawing.Point(120, 26);
+            this.isbnTextBox.Name = "isbnTextBox";
+            this.isbnTextBox.Size = new System.Drawing.Size(174, 20);
+            this.isbnTextBox.TabIndex = 3;
+            // 
+            // bookDetailsTextBox
+            // 
+            this.bookDetailsTextBox.Location = new System.Drawing.Point(120, 88);
+            this.bookDetailsTextBox.Multiline = true;
+            this.bookDetailsTextBox.Name = "bookDetailsTextBox";
+            this.bookDetailsTextBox.Size = new System.Drawing.Size(174, 53);
+            this.bookDetailsTextBox.TabIndex = 4;
+            // 
+            // isbnBox
+            // 
+            this.isbnBox.Location = new System.Drawing.Point(97, 22);
+            this.isbnBox.Name = "isbnBox";
+            this.isbnBox.Size = new System.Drawing.Size(197, 20);
+            this.isbnBox.TabIndex = 3;
+            // 
+            // bookDetailsBox
+            // 
+            this.bookDetailsBox.Location = new System.Drawing.Point(97, 77);
+            this.bookDetailsBox.Multiline = true;
+            this.bookDetailsBox.Name = "bookDetailsBox";
+            this.bookDetailsBox.Size = new System.Drawing.Size(197, 66);
+            this.bookDetailsBox.TabIndex = 4;
+         
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 416);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "BookInfoUI";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox bookDetailsTextBox;
+        private System.Windows.Forms.TextBox isbnTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button addBookButton;
+        private System.Windows.Forms.TextBox bookDetailsBox;
+        private System.Windows.Forms.TextBox isbnBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button findBookButton;
+    }
+}
+
---
+++ BookInfoApp/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BookInfoApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+      
+       private Hashtable tables=new Hashtable();
+
+        private string isbn;
+
+        private string bookDetails;
+
+        private void addBookButton_Click(object sender, EventArgs e)
+        {
+            isbn = isbnTextBox.Text;
+
+            bookDetails = bookDetailsTextBox.Text;
+
+            tables.Add(isbn,bookDetails);
+
+            isbnTextBox.Text = "";
+            bookDetailsTextBox.Text = "";
+        }
+
+        private void findBookButton_Click(object sender, EventArgs e)
+        {
+            string search = isbnBox.Text;
+
+            if (tables.ContainsKey(search))
+            {
+                bookDetailsBox.Text = tables[search].ToString();
+                
+            }
+            else
+            {
+                MessageBox.Show("Book Not Found");
+                bookDetailsBox.Text = "";
+            }
+        }
+
+      
+    }
+}
